0e5736e391b3e6e5043a460dd2b492d88e44aa8a
jlhiguitac <jluis.higuita@gmail.com>
2024-10-05 21:45:30 -0500

    chore: eliminé archivos repetidos

--- app_node_docker/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-const router = Router();
-
-router.get('/', (req, res) => res.render("index", {titulo : "Mi primera página Node"}))
-router.get('/about', (req, res) => res.render("about", {x : '35'}))
-router.get('/contact', (req, res) => res.render("contact"))
-
-export default router
